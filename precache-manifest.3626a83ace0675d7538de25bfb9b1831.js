7d253bab0070e5cf7ba3b02b6bc3e69f659383e7
kevinhooke <kevin.hooke@gmail.com>
2022-09-11 14:02:40 -0700

    Updates

---
+++ precache-manifest.3626a83ace0675d7538de25bfb9b1831.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e960265304d3f59f563a",
+    "url": "/sudoku-react-app/static/css/main.a4fd111d.chunk.css"
+  },
+  {
+    "revision": "e960265304d3f59f563a",
+    "url": "/sudoku-react-app/static/js/main.e9602653.chunk.js"
+  },
+  {
+    "revision": "38729fecbc131a2ee4df",
+    "url": "/sudoku-react-app/static/js/runtime~main.38729fec.js"
+  },
+  {
+    "revision": "cbc07976d39bccf296f1",
+    "url": "/sudoku-react-app/static/js/2.cbc07976.chunk.js"
+  },
+  {
+    "revision": "7e06d1cb67c02af33a7450b1783929e0",
+    "url": "/sudoku-react-app/index.html"
+  }
+];
